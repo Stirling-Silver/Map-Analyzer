c72eb3018fa14ee3263d06e9b84530d3ff470f24
Stirling-Silver <113132128+Stirling-Silver@users.noreply.github.com>
2022-10-20 11:22:51 -0600

    Starting

--- solutions.js
+++ solutions.js
@@ -2,79 +2,100 @@
 
 // Global Variables
 let row, column; // Row & Column of Mouse on the Map
-let outputEl = document.getElementById('currentTile');
+let outputEl = document.getElementById("currentTile");
 
 // Solution Functions...
 function level1Solution() {
   // USE BINARY IF STATEMENT
   if (column <= 4) {
-    outputEl.innerHTML = 'Sand';
+    outputEl.innerHTML = "Sand";
   } else {
-    outputEl.innerHTML = 'Water';
+    outputEl.innerHTML = "Water";
   }
 }
 
 function level2Solution() {
   // USE CHAINED IF STATEMENT
-  outputEl.innerHTML = '----';
+  if (column <= 2) {
+  outputEl.innerHTML = "Hills";
+  } else if (column <= 7) {
+    outputEl.innerHTML = "Sand";
+  } else {
+    outputEl.innerHTML = "Water";
+  }
+  
 }
 
 function level3Solution() {
   // USE BINARY IF STATEMENT
-  outputEl.innerHTML = '----';
+  if (column >= 3 && column <= 7) {
+    outputEl.innerHTML = "Sand";
+  } else {
+  outputEl.innerHTML = "Hills";
+  }
 }
 
 function level4Solution() {
   // USE BINARY IF STATEMENT
-  outputEl.innerHTML = '----';
+  if (row <= 2) { 
+  outputEl.innerHTML = "Hills";
+  } else {
+    outputEl.innerHTML = "Sand";
+  }
 }
 
 function level5Solution() {
   // USE CHAINED IF STATEMENT
-  outputEl.innerHTML = '----';
+  if (row <= 2) {
+    outputEl.innerHTML = "Hills";
+  } else if (row <= 5) {
+    outputEl.innerHTML = "Sand";
+  } else {
+  outputEl.innerHTML = "Water";
+  }
 }
 
 function level6Solution() {
   // USE BINARY IF STATEMENT
-  outputEl.innerHTML = '----';
+  outputEl.innerHTML = "----";
 }
 
 function level7Solution() {
   // USE BINARY IF STATEMENT
-  outputEl.innerHTML = '----';
+  outputEl.innerHTML = "----";
 }
 
 function level8Solution() {
   // USE BINARY IF STATEMENT
-  outputEl.innerHTML = '----';
+  outputEl.innerHTML = "----";
 }
 
 function level9Solution() {
   // USE BINARY IF STATEMENT
-  outputEl.innerHTML = '----';
+  outputEl.innerHTML = "----";
 }
 
 function level10Solution() {
   // USE CHAINED IF STATEMENT
-  outputEl.innerHTML = '----';
+  outputEl.innerHTML = "----";
 }
 
 function level11Solution() {
   // USE CHAINED IF STATEMENT
-  outputEl.innerHTML = '----';
+  outputEl.innerHTML = "----";
 }
 
 function level12Solution() {
   // USE BINARY IF STATEMENT
-  outputEl.innerHTML = '----';
+  outputEl.innerHTML = "----";
 }
 
 function level13Solution() {
   // USE BINARY IF STATEMENT
-  outputEl.innerHTML = '----';
+  outputEl.innerHTML = "----";
 }
 
 function level14Solution() {
   // USE CHAINED IF STATEMENT
-  outputEl.innerHTML = '----';
+  outputEl.innerHTML = "----";
 }
